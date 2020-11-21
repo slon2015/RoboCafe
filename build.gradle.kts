022fd7ec4c970f0880306210ce3b6bbfff68a5ea
serafim <serafim.gerasimov@mail.ru>
2020-11-21 14:13:40 +0300

    Application configuration classes fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-    id("org.springframework.boot") version "2.4.0"
+    id("org.springframework.boot") version "2.4.1-SNAPSHOT"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
     kotlin("plugin.jpa") version "1.4.10"
 }
 
-group = "com.example"
+group = "com.robocafe.all"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
--- src/main/java/com/robocafe/all/ApplicationConfiguration.kt
+++ src/main/java/com/robocafe/all/ApplicationConfiguration.kt
@@ -1,23 +1,23 @@
 package com.robocafe.all
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.context.annotation.Configuration
 
-@Configuration
-@ConfigurationProperties(prefix = "positions")
-data class PositionsConfiguration(
-        var endpoints: Set<String>?
+class PositionsConfiguration @ConstructorBinding constructor(
+        val endpoints: Set<String>
 )
 
-@Configuration
-@ConfigurationProperties(prefix = "afiches")
-data class AfichesConfiguration(
-        var endpoints: Set<String>?
+class AfichesConfiguration @ConstructorBinding constructor(
+        val endpoints: Set<String>
 )
 
 @Configuration
 @ConfigurationProperties(prefix = "application")
-class ApplicationConfiguration(
-        var positions: PositionsConfiguration,
-        var afiches: AfichesConfiguration
-)
+class ApplicationConfiguration {
+        @NestedConfigurationProperty
+        lateinit var positions: PositionsConfiguration
+        @NestedConfigurationProperty
+        lateinit var afiches: AfichesConfiguration
+}
