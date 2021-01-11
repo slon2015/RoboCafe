95364de9231dec13672b9cf79b845eadaaa24c76
serafim <serafim.gerasimov@mail.ru>
2021-01-11 15:45:10 +0300

    Change exception to runtime exception for more transparent logging

--- src/main/java/com/robocafe/all/application/security/exceptions.kt
+++ src/main/java/com/robocafe/all/application/security/exceptions.kt
@@ -2,8 +2,9 @@ package com.robocafe.all.application.security
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-open class SecurityException: Exception()
+open class SecurityException: RuntimeException()
 class SecurityObjectNotFound: SecurityException()
 class SecurityObjectInvalidated: SecurityException()
--- src/main/java/com/robocafe/all/application/services/CommonExceptions.kt
+++ src/main/java/com/robocafe/all/application/services/CommonExceptions.kt
@@ -3,9 +3,10 @@ package com.robocafe.all.application.services
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 import java.lang.Exception
+import java.lang.RuntimeException
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-open class EntityNotFound: Exception()
+open class EntityNotFound: RuntimeException()
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-open class DomainException: Exception()
+open class DomainException: RuntimeException()
